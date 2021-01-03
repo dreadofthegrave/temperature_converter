7cbffb10be9f31f4b44a6890144d086d836947eb
Piotr Paruch <pparuch@student.agh.edu.pl>
2021-01-03 18:45:41 +0100

    add project files

---
+++ lib/temperature_converter.ex
@@ -0,0 +1,18 @@
+defmodule TemperatureConverter do
+  @moduledoc """
+  Documentation for `TemperatureConverter`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TemperatureConverter.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/temperature_converter_test.exs
@@ -0,0 +1,8 @@
+defmodule TemperatureConverterTest do
+  use ExUnit.Case
+  doctest TemperatureConverter
+
+  test "greets the world" do
+    assert TemperatureConverter.hello() == :world
+  end
+end
